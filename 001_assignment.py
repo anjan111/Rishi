067218f096ad5d3c5ff9abcbddf4798371d270cb
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2020-06-03 20:56:59 +0530

    Add files via upload

---
+++ 001_assignment.py
@@ -0,0 +1,16 @@
+## addition then update
+a = input("enter a :")
+b = input("enter b :")
+print a
+print b
+a =  a+b #  addition then update
+print a
+print b
+
+a = input("enter a :")
+b = input("enter b :")
+print a
+print b
+a +=  b   #   a  =  a+b
+print a
+print b
---
+++ 002_assign_sub.py
@@ -0,0 +1,16 @@
+## difference then update
+a = input("enter a :")
+b = input("enter b :")
+print a
+print b
+a =  a - b #  subtraction then update
+print a
+print b
+
+a = input("enter a :")
+b = input("enter b :")
+print a
+print b
+a -=  b   #   a  =  a - b
+print a
+print b
